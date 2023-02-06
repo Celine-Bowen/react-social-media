513e175063e7693f72a03aa5e0e0a1a309f19d8b
Celine-Bowen <Celine-Bowen>
2023-02-06 16:01:00 +0300

    added protected route

--- src/App.js
+++ src/App.js
@@ -6,10 +6,57 @@ import {
   RouterProvider,
   Route,
   Link,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftbar/LeftBar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/register",
       element: <Register />,
